1c49a9fa352e671b65114b1815773e55dae10269
Kirill Belashov <udenjordisk@gmail.com>
2023-09-19 13:27:50 +0600

    Initial Commit

---
+++ CodePresentation/CodePresentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodePresentationApp.swift
+//  CodePresentation
+//
+//  Created by Kirill Belashov on 19/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CodePresentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
